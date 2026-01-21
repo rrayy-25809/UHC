e6e2eb1287c4e06ab459dd5cbb412b5fe27f6377
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2026-01-21 20:09:40 +0900

    Changes to be committed: Aqua에 귀속, gitignore 설정 	new file:   .gitignore 	deleted:    .gradle/8.8/checksums/checksums.lock 	deleted:    .gradle/8.8/dependencies-accessors/gc.properties 	deleted:    .gradle/8.8/executionHistory/executionHistory.bin 	deleted:    .gradle/8.8/executionHistory/executionHistory.lock 	deleted:    .gradle/8.8/fileChanges/last-build.bin 	deleted:    .gradle/8.8/fileHashes/fileHashes.bin 	deleted:    .gradle/8.8/fileHashes/fileHashes.lock 	deleted:    .gradle/8.8/gc.properties 	deleted:    .gradle/buildOutputCleanup/buildOutputCleanup.lock 	deleted:    .gradle/buildOutputCleanup/cache.properties 	deleted:    .gradle/buildOutputCleanup/outputFiles.bin 	deleted:    .gradle/file-system.probe 	deleted:    .gradle/vcs-1/gc.properties 	modified:   LICENSE 	deleted:    src/main/java/com/a/uHC/UHC.java 	new file:   src/main/java/com/aqua/uhc/UHC.java

---
+++ src/main/java/com/aqua/uhc/UHC.java
@@ -0,0 +1,16 @@
+package com.aqua.uhc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UHC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("UHC 플러그인이 활성화 되었습니다!"); // 정보, 경고
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("UHC 플러그인이 비활성화 되었습니다!");
+    }
+}
