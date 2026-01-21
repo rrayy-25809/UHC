a560f753b60a0a197e086b0bc15d2caf003a822d
kloreIla <1.g.com>
2026-01-21 19:49:49 +0900

    d

---
+++ src/main/java/com/a/uHC/UHC.java
@@ -0,0 +1,17 @@
+package com.a.uHC;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UHC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
